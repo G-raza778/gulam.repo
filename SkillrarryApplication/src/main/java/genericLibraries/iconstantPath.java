c68666832ce35fcc637de81866b2e28603e94295
G-raza778 <129850736+G-raza778@users.noreply.github.com>
2023-04-07 21:04:21 +0530

    Delete SkillrarryApplication directory

--- SkillrarryApplication/src/main/java/genericLibraries/iconstantPath.java
+++
@@ -1,6 +0,0 @@
-package genericLibraries;
-
-public interface iconstantPath {
-	String PROPERTIES_PATH ="./src/test/resources/common data.properties";
-	String EXCEL_PATH="./src/test/resources/excel1.xlsx";
-}
